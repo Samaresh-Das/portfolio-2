23ae87216e2630e24217389040e3e800726796b2
Samaresh Das <samareshmail679@gmail.com>
2023-04-28 21:09:36 +0530

    Firebase auth project added

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,7 @@ const Button = (props) => {
     <div className={`${props.classNameDiv}`}>
       <a
         href={props.path}
-        className={`${props.className} px-3 py-2 text-center bg-[#fb5607] rounded-lg text-[#F0E3CA] hover:bg-[#F0E3CA] hover:text-[#fb5607]`}
+        className={`${props.className} px-3 py-2 text-center bg-[#fb5607] rounded-lg text-[#F0E3CA] hover:bg-[#F0E3CA] hover:text-[#fb5607] `}
         target="_blank"
         rel="noopener noreferrer"
       >
